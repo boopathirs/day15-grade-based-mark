67bdfac522b2149868ae74855b6c0b5c07b21b37
bopathirs <boopathirs111@gmail.com>
2022-06-30 09:26:28 +0530

    task15

---
+++ function-grade.js
@@ -0,0 +1,19 @@
+let marks = 61;
+function grade(){
+if(40>marks){
+    console.log('student grade is F');
+}
+else if(90<marks){
+    console.log('student grade is A');
+}else if(80<marks){
+    console.log('student grade is B');
+}else if(70<marks){
+    console.log('student grade is c');
+}else if(60<marks){
+    console.log('student grade is D');
+}else if(50<marks)
+{
+    console.log('student grade is E');
+}
+}
+let details = grade();
